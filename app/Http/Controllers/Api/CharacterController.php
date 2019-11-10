16ae316f3be49c7330f2df10db2d1e2c61f5424f
carloswinker <carlos@winker.com.br>
2019-11-10 13:46:06 -0300

    adicionando ultimas alterações

--- app/Http/Controllers/Api/CharacterController.php
+++ app/Http/Controllers/Api/CharacterController.php
@@ -11,12 +11,34 @@ class CharacterController extends Controller
     public function allCharacters()
     {
         $personagens = Personagem::all();
-        return response()->json($personagens);
+        return response()->json($this->toApi($personagens));
     }
 
     public function character($id)
     {
         $personagem = Personagem::where('personagem_id',$id)->get();
         return response()->json($personagem);
+    }
+
+    private function toApi($personagens)
+    {
+
+        $return = [];
+
+        foreach ($personagens as $key => $personagem) {
+            $char["name"]       = $personagem->nome;
+            $char["age"]        = $personagem->idade;
+            $char["level"]      = $personagem->nivel;
+            $char["weight"]     = $personagem->peso;
+            $char["height"]     = $personagem->altura;
+            $char["experience"] = $personagem->experiencia;
+            $char["race"]       = $personagem->raca->nome;
+            $char["profession"] = $personagem->profissao->nome;
+            $char["religion"]   = $personagem->religiao->nome;
+
+            $return[] = $char;
+        }
+
+        return $return;
     }
 }
--- app/Personagem.php
+++ app/Personagem.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Personagem extends Model
 {
     protected $table = 'personagens';
+
+    protected function raca() {
+        return $this->belongsTo('App\Raca','raca_id','raca_id');
+    }
+
+    protected function religiao() {
+        return $this->belongsTo('App\Religiao','religiao_id','religiao_id');
+    }
+
+    protected function profissao() {
+        return $this->belongsTo('App\Profissao','profissao_id','profissao_id');
+    }
+
 }
